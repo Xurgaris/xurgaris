3f1bb9751ea825abe781a30b3c4818f159ae6a60
danielnascimentoj <167441372+Xurgaris@users.noreply.github.com>
2024-04-27 12:31:52 -0300

    alt

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,7 @@
 function clickMenu(){
     if(menu.style.display == "block"){
         menu.style.display = "none"
+        menu.style.color = "#000"
     }
     else{
         menu.style.display = "block"
