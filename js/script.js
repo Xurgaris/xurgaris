a48ca311d48e704d0d9d7b2c7b002aa1cd437653
danielnascimentoj <167441372+Xurgaris@users.noreply.github.com>
2024-04-29 23:04:55 -0300

    alt

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function clickMenu(){
+    if(menu.style.display == "block"){
+        menu.style.display = "none"
+        menu.style.color = "#fff"
+        men
+    }
+    else{
+        menu.style.display = "block"
+        menu.style.color = "#fff"
+    }
+}
